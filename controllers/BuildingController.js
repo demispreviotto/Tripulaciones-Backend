f3c0f8633928a14c0b7f6d861c5c1fcf33dcd096
nblancoz <nblancozappala@gmail.com>
2024-01-11 16:35:40 +0100

    building routes modified & controller

--- controllers/BuildingController.js
+++ controllers/BuildingController.js
@@ -1,56 +1,63 @@
 require("dotenv").config();
-const Building = require('../models/Building');
+const Building = require("../models/Building");
 
 const BuildingController = {
-    async createBuilding(req, res) {
-        try {
-            const building = new Building(req.body);
-            await building.save();
-            res.status(201).json(building);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    },
-    async getAllBuildings(req, res) {
-        try {
-            const buildings = await Building.find();
-            res.json(buildings);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    },
-    async getBuildingById(req, res) {
-        try {
-            const building = await Building.findById(req.params.id);
-            if (!building) {
-                return res.status(404).json({ error: 'Building not found' });
-            }
-            res.json(building);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
-    },
-    async updateBuildingById(req, res) {
-        try {
-            const building = await Building.findByIdAndUpdate(req.params.id, req.body, { new: true });
-            if (!building) {
-                return res.status(404).json({ error: 'Building not found' });
-            }
-            res.json(building);
-        } catch (error) {
-            res.status(400).json({ error: error.message });
-        }
-    },
-    async deleteBuildingById(req, res) {
-        try {
-            const building = await Building.findByIdAndDelete(req.params.id);
-            if (!building) {
-                return res.status(404).json({ error: 'Building not found' });
-            }
-            res.json(building);
-        } catch (error) {
-            res.status(500).json({ error: error.message });
-        }
+  async createBuilding(req, res) {
+    try {
+      const building = new Building(req.body);
+      await building.save();
+      res.status(201).json(building);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
     }
+  },
+  async getAllBuildings(req, res) {
+    try {
+      const buildings = await Building.find();
+      res.json(buildings);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  async getBuildingById(req, res) {
+    try {
+      const building = await Building.findById(req.params.id);
+      if (!building) {
+        return res.status(404).json({ error: "Building not found" });
+      }
+      res.json(building);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  async updateBuildingById(req, res) {
+    try {
+      const building = await Building.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+      if (!building) {
+        return res.status(404).json({ error: "Building not found" });
+      }
+      res.json(building);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  },
+  async deleteBuildingById(req, res) {
+    try {
+      const building = await Building.findByIdAndDelete(req.params.id);
+      res
+        .status(200)
+        .send({ message: "Building deleted successfully", building });
+      if (!building) {
+        return res.status(404).json({ error: "Building not found" });
+      }
+      res.json(building);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 module.exports = BuildingController;
