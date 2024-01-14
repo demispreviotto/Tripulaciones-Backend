8801254a0d00fcae2be20bb39dae3c87ab837ee4
Demis Previotto <demis.previotto@gmail.com>
2024-01-14 09:52:49 +0100

    rename the buildings routes to match with frontEnd

--- routes/buildings.js
+++ routes/buildings.js
@@ -4,14 +4,10 @@ const router = express.Router();
 const BuildingController = require("../controllers/BuildingController");
 const { authentication } = require("../middleware/authentication");
 
-router.post("/create", authentication, BuildingController.createBuilding);
-router.get("/getAll", authentication, BuildingController.getAllBuildings);
-router.get("/getById/:id", authentication, BuildingController.getBuildingById);
-router.put(
-  "/update/:id",
-  authentication,
-  BuildingController.updateBuildingById
-);
-router.delete("/delete/:id", BuildingController.deleteBuildingById);
+router.post("/createBuilding", authentication, BuildingController.createBuilding);
+router.get("/getAllBuildings", authentication, BuildingController.getAllBuildings);
+router.get("/getBuildingById/:id", authentication, BuildingController.getBuildingById);
+router.put("/updateBuilding/:id", authentication, BuildingController.updateBuildingById);
+router.delete("/deleteBuildingById/:id", authentication, BuildingController.deleteBuildingById);
 
 module.exports = router;
