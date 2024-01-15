b0f45a6f2d16c3f3e28edb4febe1e3bb7cc63d3e
Demis Previotto <demis.previotto@gmail.com>
2024-01-15 13:15:25 +0100

    fetch incidence data from data analist api

---
+++ seeders/fetchIncidenceData.js
@@ -0,0 +1,23 @@
+const axios = require('axios')
+const Incidence = require('../models/Incidence');
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+    .then(() => console.log('mongoDB connected'))
+    .catch(err => console.error(err))
+
+axios.get(process.env.DATA_URI)
+    .then(async res => {
+        console.log(res.data)
+        const incidences = res.data.incidences
+        for (const incidence of incidences) {
+            Incidence.create({
+                summary: incidence.mensaje,
+                category: incidence.categoria,
+                originalMessage: incidence.mensaje,
+                status: incidence.estado,
+                ownerIds: incidence.owner,
+            })
+        }
+    })
