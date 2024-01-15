02e5e98b14a80edcbea58fdd2daddfc1ab0653e5
nblancoz <nblancozappala@gmail.com>
2024-01-15 14:05:07 +0100

    added the buildingId to the scheme

--- models/Todo.js
+++ models/Todo.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const ObjectId = mongoose.Types.ObjectId;
 
 const TodoSchema = new mongoose.Schema(
     {
@@ -13,6 +14,7 @@ const TodoSchema = new mongoose.Schema(
             type: Boolean,
             default: false,
         },
+        buildingId: [{ type: ObjectId, ref: "Building" }]
     },
     { timestamps: true }
 );
