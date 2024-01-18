b489a6d520c6eb827567cc9b820d6e9daa347477
Demis Previotto <demis.previotto@gmail.com>
2024-01-18 13:02:06 +0100

    admin routes error fixed

--- index.js
+++ index.js
@@ -4,13 +4,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const cors = require("cors");
 
-const Building = require("./models/Building");
-const Owner = require("./models/Owner");
-const jsonFincasData = require("./data/json_fincas.json");
-
 const { handleTypeError } = require("./middleware/errors");
 const { dbConnection } = require("./config/config");
-const Incidence = require("./models/Incidence");
 dbConnection();
 
 app.use(express.json());
@@ -23,7 +18,7 @@ app.use("/buildings", require("./routes/buildings"));
 app.use("/doors", require("./routes/doors"));
 app.use("/services", require("./routes/services"));
 app.use("/todos", require("./routes/todos"));
-app.use("/admin", require("./routes/admin"))
+app.use("/admin", require("./routes/admin"));
 
 app.use(handleTypeError);
 
--- routes/admin.js
+++ routes/admin.js
@@ -4,9 +4,8 @@ const router = express.Router();
 const AdminController = require("../controllers/AdminController");
 const { authentication, isAdmin } = require("../middleware/authentication");
 
-router.post("/fetchAndCreateIncidences", authentication, isAdmin, AdminController.fetchAndCreateIncidences)
-router.post("/createBuildingsFromJson", authentication, isAdmin, AdminController.fetchAndCreateIncidences)
-router.post("/createOwnersFromJson", authentication, isAdmin, AdminController.fetchAndCreateIncidences)
-router.post("/mapIncidencesToBuildings", authentication, isAdmin, AdminController.fetchAndCreateIncidences)
+router.post("/createBuildingsFromJson", authentication, isAdmin, AdminController.createBuildingsFromJson);
+router.post("/createOwnersFromJson", authentication, isAdmin, AdminController.createOwnersFromJson);
+router.post("/mapIncidencesToBuildings", authentication, isAdmin, AdminController.mapIncidencesToBuildings);
 
 module.exports = router;
